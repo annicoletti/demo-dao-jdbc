d8182d28fc341a3d5f02c928666259599c08899d
André Nicoletti <andre.nicoletti@live.com>
2019-01-04 12:12:32 -0200

    fyndAll Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,12 @@ public class Program {
         List<Seller> list = sellerDao.findByDepartment(department);
         for(Seller obj : list){
             System.out.println(obj);
-        }       
+        }  
+        
+        System.out.println("=== TEST 3: seller findAll ===");
+        list = sellerDao.findAll();
+        for(Seller obj : list){
+            System.out.println(obj);
+        } 
     }
 }
